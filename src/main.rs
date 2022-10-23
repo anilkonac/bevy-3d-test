2e8e210855d1e9a5f3319ecec2f919d78fd239e1
Anıl Konaç <anil.konac@gmail.com>
2022-10-24 02:07:56 +0300

    Create a simple scene
    A ground plane and a cube sitting on it.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,54 @@
 use bevy::prelude::*;
 
-fn hello() {
-    println!("Hello World!");
-}
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(hello)
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // ground plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+        material: materials.add(Color::GRAY.into()),
+        ..default()
+    });
+
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::TEAL.into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+
+    // light
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 80000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(-4.0, 10.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // Camera
+    let camera_height = 1.5;
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-5.0, camera_height, -4.0).looking_at(
+            Vec3 {
+                x: 0.0,
+                y: camera_height,
+                z: 0.0,
+            },
+            Vec3::Y,
+        ),
+        ..default()
+    });
 }
