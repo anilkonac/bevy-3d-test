9e6eb8f9a53eb056fe9acdbe36ef11ca0e433de1
Anıl Konaç <anil.konac@gmail.com>
2022-10-26 00:50:51 +0300

    [WIP] Implement mouse look
    Current implementation makes the player a fly camera. Rendering is not 
    fluid while moving the mouse, and there are problems with the rotation 
    axes.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use bevy::{prelude::*, time::FixedTimestep};
+use bevy::{
+    // diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    input::mouse::MouseMotion,
+    prelude::*,
+    time::FixedTimestep,
+};
 
 const TIME_STEP: f32 = 1.0 / 60.0;
 const PLAYER_SPEED: f32 = 3.0;
@@ -7,13 +12,21 @@ const KEYS_BACKWARD: [KeyCode; 2] = [KeyCode::S, KeyCode::Down];
 const KEYS_RIGHT: [KeyCode; 2] = [KeyCode::D, KeyCode::Right];
 const KEYS_LEFT: [KeyCode; 2] = [KeyCode::A, KeyCode::Left];
 
+// Resource
+struct MouseGrabbed(bool);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        // .add_plugin(LogDiagnosticsPlugin::default())
+        // .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .insert_resource(MouseGrabbed(false))
         .add_startup_system(setup)
+        .add_system(grab_mouse)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
+                .with_system(player_look_system)
                 .with_system(player_movement_system),
         )
         .run();
@@ -26,7 +39,15 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut windows: ResMut<Windows>,
+    mut mouse_grabbed: ResMut<MouseGrabbed>,
 ) {
+    // Grab mouse
+    let window = windows.get_primary_mut().unwrap();
+    window.set_cursor_visibility(false);
+    window.set_cursor_lock_mode(true);
+    mouse_grabbed.0 = true;
+
     // ground plane
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
@@ -102,4 +123,43 @@ fn player_movement_system(
 
     // Apply translation
     transform.translation += movement_direction * PLAYER_SPEED * TIME_STEP;
+}
+
+fn player_look_system(
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+    let mut delta = Vec2::ZERO;
+    for event in mouse_motion_events.iter() {
+        delta += event.delta;
+    }
+    // println!("{:?}", delta);
+
+    let mut transform = query.single_mut();
+    transform.rotate_y(-delta.x.to_radians());
+    transform.rotate_local_x(-delta.y.to_radians());
+    // transform.rotate_local(Quat::from_euler(
+    //     EulerRot::YXZ,
+    //     -delta.x.to_radians(),
+    //     -delta.y.to_radians(),
+    //     0.0,
+    // ));
+}
+
+fn grab_mouse(
+    mut windows: ResMut<Windows>,
+    mouse: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+    mut mouse_grabbed: ResMut<MouseGrabbed>,
+) {
+    let window = windows.get_primary_mut().unwrap();
+    if !mouse_grabbed.0 && mouse.just_pressed(MouseButton::Left) {
+        window.set_cursor_visibility(false);
+        window.set_cursor_lock_mode(true);
+        mouse_grabbed.0 = true;
+    } else if mouse_grabbed.0 && key.just_pressed(KeyCode::Escape) {
+        window.set_cursor_visibility(true);
+        window.set_cursor_lock_mode(false);
+        mouse_grabbed.0 = false;
+    }
 }
