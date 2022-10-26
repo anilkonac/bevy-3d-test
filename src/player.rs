bffcd17926c103607444ef41a68f1801ba3440d7
Anıl Konaç <anil.konac@gmail.com>
2022-10-27 01:06:54 +0300

    Implement up/down movement for the flying camera

--- src/player.rs
+++ src/player.rs
@@ -7,6 +7,8 @@ const KEYS_FORWARD: [KeyCode; 2] = [KeyCode::W, KeyCode::Up];
 const KEYS_BACKWARD: [KeyCode; 2] = [KeyCode::S, KeyCode::Down];
 const KEYS_RIGHT: [KeyCode; 2] = [KeyCode::D, KeyCode::Right];
 const KEYS_LEFT: [KeyCode; 2] = [KeyCode::A, KeyCode::Left];
+const KEY_UP: KeyCode = KeyCode::E;
+const KEY_DOWN: KeyCode = KeyCode::Q;
 const MOUSE_SENSITIVITY: f32 = 0.15;
 
 pub struct PlayerPlugin;
@@ -32,30 +34,38 @@ fn player_move_system(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<&mut Transform, With<Player>>,
 ) {
-    let mut movement_factor_forward = 0.0;
-    let mut movement_factor_right = 0.0;
+    // let mut movement_factor_forward = 0.0;
+    // let mut movement_factor_right = 0.0;
+    let mut movement_axes = Vec3::ZERO;
     if keyboard_input.any_pressed(KEYS_FORWARD) {
-        movement_factor_forward += 1.0
+        movement_axes.z += 1.0
     }
     if keyboard_input.any_pressed(KEYS_BACKWARD) {
-        movement_factor_forward -= 1.0;
+        movement_axes.z -= 1.0;
     }
     if keyboard_input.any_pressed(KEYS_RIGHT) {
-        movement_factor_right += 1.0;
+        movement_axes.x += 1.0;
     }
     if keyboard_input.any_pressed(KEYS_LEFT) {
-        movement_factor_right += -1.0;
+        movement_axes.x += -1.0;
+    }
+    if keyboard_input.pressed(KEY_UP) {
+        movement_axes.y += 1.0;
+    }
+    if keyboard_input.pressed(KEY_DOWN) {
+        movement_axes.y += -1.0;
     }
 
-    if (movement_factor_forward == 0.0) && (movement_factor_right == 0.0) {
+    if movement_axes == Vec3::ZERO {
         return;
     }
 
     let mut transform = query.single_mut();
 
     //  Calculate movement direction
-    let movement_direction =
-        movement_factor_forward * transform.forward() + movement_factor_right * transform.right();
+    let movement_direction = movement_axes.z * transform.forward()
+        + movement_axes.x * transform.right()
+        + movement_axes.y * transform.up();
 
     let movement_direction = movement_direction.normalize();
 
