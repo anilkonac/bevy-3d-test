0cc32401bb6e0e76121175f4bf8fb6a6ca21524b
Anıl Konaç <anil.konac@gmail.com>
2022-11-03 17:32:43 +0300

    Set shadow map slider to show point light as well

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,7 @@
-use bevy::{pbr::DirectionalLightShadowMap, prelude::*};
+use bevy::{
+    pbr::{DirectionalLightShadowMap, PointLightShadowMap},
+    prelude::*,
+};
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 
 use crate::AppState;
@@ -15,6 +18,7 @@ impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Msaa::default())
             .insert_resource(DirectionalLightShadowMap::default())
+            .insert_resource(PointLightShadowMap::default())
             .add_plugin(EguiPlugin)
             .add_system_set(SystemSet::on_update(AppState::Menu).with_system(ui_graphics));
     }
@@ -23,12 +27,13 @@ impl Plugin for UIPlugin {
 fn ui_graphics(
     mut egui_context: ResMut<EguiContext>,
     mut msaa: ResMut<Msaa>,
-    mut shadop_map: ResMut<DirectionalLightShadowMap>,
+    mut shadow_map_direct: ResMut<DirectionalLightShadowMap>,
+    mut shadow_map_point: ResMut<PointLightShadowMap>,
     mut query_light_direct: Query<&mut DirectionalLight>,
     mut query_light_point: Query<&mut PointLight>,
 ) {
     egui::Window::new("Graphics").show(egui_context.ctx_mut(), |ui| {
-        const STEP_SIZE_SHADOW_MAP: usize = 2048;
+        const STEP_SIZE_SHADOW_MAP: usize = 1024;
 
         let mut msaa_active = msaa.samples > 1;
         ui.checkbox(&mut msaa_active, "MSAA");
@@ -53,23 +58,25 @@ fn ui_graphics(
             ui.label("Light Type");
         });
 
-        match light_type {
+        let shadow_map_size = match light_type {
             LightType::Point => {
                 light_direct.illuminance = 0.0;
                 light_point.intensity = 800.0;
+                &mut shadow_map_point.size
             }
             LightType::Directional => {
                 light_direct.illuminance = 100000.0;
                 light_point.intensity = 0.0;
+                &mut shadow_map_direct.size
             }
-        }
+        };
 
         ui.separator();
 
         ui.add(
             egui::Slider::new(
-                &mut shadop_map.size,
-                STEP_SIZE_SHADOW_MAP..=STEP_SIZE_SHADOW_MAP * 8,
+                shadow_map_size,
+                STEP_SIZE_SHADOW_MAP..=STEP_SIZE_SHADOW_MAP * 10,
             )
             .step_by(STEP_SIZE_SHADOW_MAP as f64)
             .text("Shadow Map Size"),
